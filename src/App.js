7836ec35aaa3235a020f249cfdbe780397057076
Milos1988 <vojvoda1988>
2022-08-01 13:20:46 +0200

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./component/Header";
 import Input from "./component/Input";
 import TodoList from "./component/TodoList";
 
 function App() {
   const [todo, setTodo] = useState([]);
+
+  // useEffect(() => {
+  //   const data = localStorage.getItem("todo");
+  //   if (data) {
+  //     setTodo(JSON.parse(data));
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
+  useEffect(() => {
+    console.log(localStorage.getItem("todo"));
+  }, [todo]);
 
   const markText = (index) => {
     todo[index].mark = !todo[index].mark;
